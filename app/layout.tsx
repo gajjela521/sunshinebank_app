0fc5c8b25425379d53043c2abecc1feba77cb497
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2024-09-29 15:58:54 -0400

    implement routing groups for home and auth page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,16 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Inter,IBM_Plex_Serif} from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({subsets:['latin'], variable:'--front-inter'});
+const ibmPlexSerif=IBM_Plex_Serif({subsets:['latin'], variable:'--font-ibm-plex-serif',weight:['500','700']});
 
 export const metadata: Metadata = {
   title: "SunShineBank",
   description: "Modern Banking Platform for Texans",
+  icons:{
+    icon:'icons/logo.svg'
+  }
 };
 
 export default function RootLayout({
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${ibmPlexSerif.variable} antialiased`}
       >
         {children}
       </body>
